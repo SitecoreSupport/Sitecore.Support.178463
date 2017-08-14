d7c091c494764e6ce95f26a6a1dfd15764080d6f
dk\ivd <ivd@sitecore.net>
2017-08-14 14:52:53 +0800

    #178463: Resolve the issue

---
+++ src/Sitecore.Support.178463/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.178463")]
+[assembly: AssemblyProduct("Sitecore.Support.178463")]
+[assembly: ComVisible(false)]
