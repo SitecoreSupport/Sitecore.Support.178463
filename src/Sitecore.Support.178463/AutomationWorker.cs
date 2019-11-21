64dc2ec465db2774a51e8a2246061edf4114510b
AleksandrChykun <aec@sitecore.net>
2019-11-21 11:10:41 +0200

    commit patch

---
+++ src/Sitecore.Support.178463/AutomationWorker.cs
@@ -0,0 +1,75 @@
+﻿
+
+namespace Sitecore.Support.Analytics.Automation
+{
+    using System.Threading;
+    using Diagnostics;
+    using Services;
+    using Sitecore.Xdb.Configuration;
+    using Threading;
+
+    public class AutomationWorker : Sitecore.Analytics.Automation.AutomationWorker
+    {
+        // Covers the 'alarm' field of the base class. It is safe because its usages are all written (Start and Stop) 
+        private AlarmClock alarm;
+
+        private static int activeThreads;
+
+
+        public override bool Start()
+        {
+            if (!XdbSettings.Enabled)
+            {
+                Log.Info("AutomationWorker was not started as xDB is disabled.", this);
+                return false;
+            }
+
+            this.alarm = new AlarmClock(this.Interval);
+            this.alarm.Ring += delegate { this.WakeupV2(); };
+
+            return true;
+        }
+
+        public override void Stop()
+        {
+            if (this.alarm != null)
+            {
+                this.alarm.Dispose();
+            }
+        }
+
+        public new static int ActiveThreads
+        {
+            get
+            {
+                return activeThreads;
+            }
+        }
+
+
+        internal void WakeupV2()
+        {
+            for (var i = 0; i < this.ThreadsCount - activeThreads; i++)
+            {
+                ManagedThreadPool.QueueUserWorkItem(state =>
+                {
+                    Interlocked.Increment(ref activeThreads);
+                    try
+                    {
+                        this.CleanUpThreadContext();
+                        Process();
+                    }
+                    finally
+                    {
+                        Interlocked.Decrement(ref activeThreads);
+                    }
+                });
+            }
+        }
+
+        protected virtual void CleanUpThreadContext()
+        {
+            Sitecore.Context.Items.Clear();
+        }
+    }
+}
